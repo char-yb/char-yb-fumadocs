060c6ebbfa7c3a28a814a3d300201837b0d1a351
ybchar <uiurihappy@naver.com>
2025-04-28 16:47:49 +0900

    feat: blog test move-to-new-blog

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,10 +7,10 @@ export default function HomePage() {
       <p className="text-fd-muted-foreground">
         You can open{' '}
         <Link
-          href="/docs"
+          href="/tech-blog"
           className="text-fd-foreground font-semibold underline"
         >
-          /docs
+          /tech-blog
         </Link>{' '}
         and see the documentation.
       </p>
