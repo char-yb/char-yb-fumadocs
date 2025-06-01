c5114ac4baa3f6a7e425bc1cd75e160658dad7ac
ybchar <uiurihappy@naver.com>
2025-06-01 15:45:48 +0900

    chore: Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,15 +3,19 @@ import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
 import { Analytics } from "@vercel/analytics/next"
+import GoogleAnalytics from "../lib/GoogleAnalytics";
 
 const inter = Inter({
   subsets: ['latin'],
 });
 
+const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || '';
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
+        <GoogleAnalytics gaId={GA_ID} />
         <RootProvider>{children}</RootProvider>
         <Analytics />
       </body>
